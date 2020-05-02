19111d6ff5b6c5cf126ebcfb4b087928ebfb2d4f
Matt Lacey <matt@mrlacey.co.uk>
2020-05-02 16:29:10 +0100

    example code

---
+++ XamlCompletionExample/ExampleXamlCompletionSource.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace XamlCompletionExample
+{
+    public class ExampleXamlCompletionSource : ICompletionSource
+    {
+        private bool _isDisposed;
+        private readonly ExampleXamlCompletionSourceProvider sourceProvider;
+        private readonly ITextBuffer textBuffer;
+
+        public ExampleXamlCompletionSource(ExampleXamlCompletionSourceProvider sourceProvider, ITextBuffer textBuffer)
+        {
+            this.sourceProvider = sourceProvider;
+            this.textBuffer = textBuffer;
+        }
+
+        public void AugmentCompletionSession(ICompletionSession session, IList<CompletionSet> completionSets)
+        {
+            if (_isDisposed) return;
+
+            var currentPoint = (session.TextView.Caret.Position.BufferPosition) - 1;
+            var navigator = this.sourceProvider.NavigatorService.GetTextStructureNavigator(this.textBuffer);
+            var extent = navigator.GetExtentOfWord(currentPoint);
+            var currentlyEnteredSpan = currentPoint.Snapshot.CreateTrackingSpan(extent.Span, SpanTrackingMode.EdgeInclusive);
+
+            // A simple example of adding something
+            // Obviously, in a real implementation this would look at various factors to produce appropriate entries.
+            var mySuggestion = new Completion("My Suggestion", "{SomethingAmazing}", string.Empty, null, string.Empty);
+
+            // Can't add to what the XamlLanguageService provides (as it's readonly)
+            // So create own set to display option(s)
+            var completions = new ObservableCollection<Completion>
+            {
+                mySuggestion
+            };
+
+            CompletionSet set = new CompletionSet("Demo", "Demo", currentlyEnteredSpan, completions, Enumerable.Empty<Completion>());
+            completionSets.Add(set);
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed) return;
+            GC.SuppressFinalize(this);
+            _isDisposed = true;
+        }
+    }
+}
---
+++ XamlCompletionExample/ExampleXamlCompletionSourceProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace XamlCompletionExample
+{
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("xaml")]
+    [Order(After = "default")]
+    [Name("XAML Intellisense Limitation Example")]
+    public class ExampleXamlCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new ExampleXamlCompletionSource(this, textBuffer);
+        }
+    }
+
+}
